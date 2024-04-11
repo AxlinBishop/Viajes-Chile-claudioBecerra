cfed96eec6efc9933e2aeded5c67c7268bd40ac1
Claudio Becerra <sacredbishop15@gmail.com>
2024-04-11 18:23:37 -0400

    Agregamos un tooltip al btn submit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){/*  todas las funciones comenzarán luego de que la página esté cargada */
+
+    /* Tooltip de Jquery */
+    $( function() { /* si colocamos title="texto" en alguna etiqueta, el texto se mostrará cómo tooltip */
+    $( document ).tooltip();
+} );
+
+
+
+});
